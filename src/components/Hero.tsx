1112d9574f14e07be7618a9cca8a4d4000911a7f
Yibo <yibowen@usc.edu>
2023-09-26 18:59:07 -0500

    add features

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -20,20 +20,12 @@ export function Hero() {
   const [callOption, setCallOption] = useState<'general' | 'demo'>('general')
 
   return (
-    <Container className="pb-24 pt-16 text-center lg:pt-24">
+    <Container className="pb-24 pt-16 text-center lg:pt-24 bg-gradient-to-b from-white to-slate-100/80">
       <div className="flex flex-col items-center gap-16 lg:flex-row">
         <div className="flex lg:basis-1/2 flex-col max-w-2xl items-center text-center lg:items-start lg:text-left">
           <h1 className="w-full font-display text-4xl font-semibold text-slate-900 sm:text-6xl">
             AI voice agents{' '}
             <span className="relative whitespace-nowrap text-main-500">
-              <svg
-                aria-hidden="true"
-                viewBox="0 0 418 42"
-                className="absolute left-0 top-2/3 h-[0.58em] w-full fill-main-300/70"
-                preserveAspectRatio="none"
-              >
-                <path d="M203.371.916c-26.013-2.078-76.686 1.963-124.73 9.946L67.3 12.749C35.421 18.062 18.2 21.766 6.004 25.934 1.244 27.561.828 27.778.874 28.61c.07 1.214.828 1.121 9.595-1.176 9.072-2.377 17.15-3.92 39.246-7.496C123.565 7.986 157.869 4.492 195.942 5.046c7.461.108 19.25 1.696 19.17 2.582-.107 1.183-7.874 4.31-25.75 10.366-21.992 7.45-35.43 12.534-36.701 13.884-2.173 2.308-.202 4.407 4.442 4.734 2.654.187 3.263.157 15.593-.78 35.401-2.686 57.944-3.488 88.365-3.143 46.327.526 75.721 2.23 130.788 7.584 19.787 1.924 20.814 1.98 24.557 1.332l.066-.011c1.201-.203 1.53-1.825.399-2.335-2.911-1.31-4.893-1.604-22.048-3.261-57.509-5.556-87.871-7.36-132.059-7.842-23.239-.254-33.617-.116-50.627.674-11.629.54-42.371 2.494-46.696 2.967-2.359.259 8.133-3.625 26.504-9.81 23.239-7.825 27.934-10.149 28.304-14.005.417-4.348-3.529-6-16.878-7.066Z" />
-              </svg>
               <span className="relative">made simple</span>
             </span>{' '}
             for small businesses.
@@ -111,7 +103,7 @@ export function Hero() {
                     Demo Scheduling
                   </button>
                   <span
-                    className="absolute top-0 block h-full rounded-md bg-white transition-all duration-300"
+                    className="absolute top-0 block h-full rounded-md bg-white transition-all duration-300 shadow"
                     ref={callOptionRef}
                     style={{ left: '0%', width: '50%' }}
                   ></span>
@@ -140,7 +132,7 @@ export function Hero() {
                 3. Tell us a bit about yourself
               </div>
               <textarea
-                placeholder="e.g. I'm a new grad from Hogwarts"
+                placeholder="e.g. I'm Tom, a new grad from Hogwarts"
                 className="max-h-[80px] min-h-[80px] w-full rounded-md bg-white/10 px-4 !outline-none !ring-transparent transition-all border-gray-200 focus:border-gray-200 focus:outline-none"
                 style={{ height: '120px' }}
               ></textarea>
--- src/components/PrimaryFeatures(Original).tsx
+++ src/components/PrimaryFeatures(Original).tsx
@@ -1,158 +1,298 @@
 'use client'
 
-import { useEffect, useState } from 'react'
-import Image from 'next/image'
+import { useEffect, useId, useRef } from 'react'
+import Image, { type ImageProps } from 'next/image'
 import { Tab } from '@headlessui/react'
 import clsx from 'clsx'
 
 import { Container } from '@/components/Container'
-import backgroundImage from '@/images/background-features.jpg'
-import screenshotExpenses from '@/images/screenshots/expenses.png'
-import screenshotPayroll from '@/images/screenshots/payroll.png'
-import screenshotReporting from '@/images/screenshots/reporting.png'
-import screenshotVatReturns from '@/images/screenshots/vat-returns.png'
+import screenshotContacts from '@/images/screenshots/contacts.png'
+import screenshotInventory from '@/images/screenshots/inventory.png'
+import screenshotProfitLoss from '@/images/screenshots/profit-loss.png'
 
-const features = [
+interface Feature {
+  name: React.ReactNode
+  summary: string
+  description: string
+  image: ImageProps['src']
+  icon: React.ComponentType
+  mobile?: boolean
+  ref?: React.RefObject<HTMLDivElement>
+}
+
+const features: Array<Feature> = [
   {
-    title: 'Payroll',
+    name: 'Reporting',
+    summary: 'Stay on top of things with always up-to-date reporting features.',
     description:
-      "Keep track of everyone's salaries and whether or not they've been paid. Direct deposit not supported.",
-    image: screenshotPayroll,
+      'We talked about reporting in the section above but we needed three items here, so mentioning it one more time for posterity.',
+    image: screenshotProfitLoss,
+    icon: function ReportingIcon() {
+      let id = useId()
+      return (
+        <>
+          <defs>
+            <linearGradient
+              id={id}
+              x1="11.5"
+              y1={18}
+              x2={36}
+              y2="15.5"
+              gradientUnits="userSpaceOnUse"
+            >
+              <stop offset=".194" stopColor="#fff" />
+              <stop offset={1} stopColor="#ffd5d0" />
+            </linearGradient>
+          </defs>
+          <path
+            d="m30 15-4 5-4-11-4 18-4-11-4 7-4-5"
+            stroke={`url(#${id})`}
+            strokeWidth={2}
+            strokeLinecap="round"
+            strokeLinejoin="round"
+          />
+        </>
+      )
+    },
   },
   {
-    title: 'Claim expenses',
+    name: 'Inventory',
+    summary:
+      'Never lose track of what’s in stock with accurate inventory tracking.',
     description:
-      "All of your receipts organized into one place, as long as you don't mind typing in the data by hand.",
-    image: screenshotExpenses,
+      'We don’t offer this as part of our software but that statement is inarguably true. Accurate inventory tracking would help you for sure.',
+    image: screenshotInventory,
+    icon: function InventoryIcon() {
+      return (
+        <>
+          <path
+            opacity=".5"
+            d="M8 17a1 1 0 0 1 1-1h18a1 1 0 0 1 1 1v2a1 1 0 0 1-1 1H9a1 1 0 0 1-1-1v-2Z"
+            fill="#fff"
+          />
+          <path
+            opacity=".3"
+            d="M8 24a1 1 0 0 1 1-1h18a1 1 0 0 1 1 1v2a1 1 0 0 1-1 1H9a1 1 0 0 1-1-1v-2Z"
+            fill="#fff"
+          />
+          <path
+            d="M8 10a1 1 0 0 1 1-1h18a1 1 0 0 1 1 1v2a1 1 0 0 1-1 1H9a1 1 0 0 1-1-1v-2Z"
+            fill="#fff"
+          />
+        </>
+      )
+    },
   },
   {
-    title: 'VAT handling',
+    name: 'Contacts',
+    summary:
+      'Organize all of your contacts, service providers, and invoices in one place.',
     description:
-      "We only sell our software to companies who don't deal with VAT at all, so technically we do all the VAT stuff they need.",
-    image: screenshotVatReturns,
-  },
-  {
-    title: 'Reporting',
-    description:
-      'Easily export your data into an Excel spreadsheet where you can do whatever the hell you want with it.',
-    image: screenshotReporting,
+      'This also isn’t actually a feature, it’s just some friendly advice. We definitely recommend that you do this, you’ll feel really organized and professional.',
+    image: screenshotContacts,
+    icon: function ContactsIcon() {
+      return (
+        <>
+          <path
+            opacity=".5"
+            d="M25.778 25.778c.39.39 1.027.393 1.384-.028A11.952 11.952 0 0 0 30 18c0-6.627-5.373-12-12-12S6 11.373 6 18c0 2.954 1.067 5.659 2.838 7.75.357.421.993.419 1.384.028.39-.39.386-1.02.036-1.448A9.959 9.959 0 0 1 8 18c0-5.523 4.477-10 10-10s10 4.477 10 10a9.959 9.959 0 0 1-2.258 6.33c-.35.427-.354 1.058.036 1.448Z"
+            fill="#fff"
+          />
+          <path
+            d="M12 28.395V28a6 6 0 0 1 12 0v.395A11.945 11.945 0 0 1 18 30c-2.186 0-4.235-.584-6-1.605ZM21 16.5c0-1.933-.5-3.5-3-3.5s-3 1.567-3 3.5 1.343 3.5 3 3.5 3-1.567 3-3.5Z"
+            fill="#fff"
+          />
+        </>
+      )
+    },
   },
 ]
 
-export function PrimaryFeatures() {
-  let [tabOrientation, setTabOrientation] = useState<'horizontal' | 'vertical'>(
-    'horizontal',
-  )
+function Feature({
+  feature,
+  isActive,
+  mobile,
+  className,
+  ...props
+}: React.ComponentPropsWithoutRef<'div'> & {
+  feature: Feature
+  isActive: boolean
+  mobile: boolean
+}) {
 
-  useEffect(() => {
-    let lgMediaQuery = window.matchMedia('(min-width: 1024px)')
+  const progressRef = useRef<HTMLDivElement>(null)
+  const progressTargetRef = useRef<HTMLDivElement>(null)
 
-    function onMediaQueryChange({ matches }: { matches: boolean }) {
-      setTabOrientation(matches ? 'vertical' : 'horizontal')
-    }
+  // print progressRef width every 1 second
+  // useEffect(() => {
+  //   const interval = setInterval(() => {
+  //     if (!mobile && isActive && progressRef.current && progressTargetRef.current) {
+  //       if (progressRef.current.offsetWidth === progressTargetRef.current.offsetWidth) {
+  //         const activeIndex = features.findIndex(f => f.summary === feature.summary)
+  //         features[(activeIndex+1)%3].ref?.current?.click()
+  //         clearInterval(interval)
+  //       }
+  //     }
+  //   }, 500)
 
-    onMediaQueryChange(lgMediaQuery)
-    lgMediaQuery.addEventListener('change', onMediaQueryChange)
-
-    return () => {
-      lgMediaQuery.removeEventListener('change', onMediaQueryChange)
-    }
-  }, [])
+  //   return () => clearInterval(interval)
+  // }, [isActive])
 
   return (
-    <section
-      id="features"
-      aria-label="Features for running your books"
-      className="relative overflow-hidden bg-blue-600 pb-28 pt-20 sm:py-32"
+    <div
+      className={clsx(
+        className,
+        'relative overflow-hidden rounded-xl border border-2 p-6 shadow-md',
+        isActive && 'border-slate-200 bg-white',
+        !isActive &&
+          'border-slate-200/30 opacity-75 transition-all duration-200 hover:opacity-100',
+      )}
+      {...props}
     >
-      <Image
-        className="absolute left-1/2 top-1/2 max-w-none translate-x-[-44%] translate-y-[-42%]"
-        src={backgroundImage}
-        alt=""
-        width={2245}
-        height={1636}
-        unoptimized
-      />
-      <Container className="relative">
-        <div className="max-w-2xl md:mx-auto md:text-center xl:max-w-none">
-          <h2 className="font-display text-3xl  text-white sm:text-4xl md:text-5xl">
-            Everything you need to run your books.
+      <div className="flex items-center gap-4">
+        <div
+          className={clsx(
+            'w-9 rounded-lg',
+            isActive ? 'bg-main-500' : 'bg-slate-500',
+          )}
+        >
+          <svg aria-hidden="true" className="h-9 w-9" fill="none">
+            <feature.icon />
+          </svg>
+        </div>
+        <h3
+          className={clsx(
+            'font-display text-lg font-[530]',
+            isActive ? 'text-main-500' : 'text-slate-600',
+          )}
+        >
+          {feature.name}
+        </h3>
+      </div>
+      <p className="mt-4 font-sans text-base leading-6 text-slate-600">
+        {feature.summary}
+      </p>
+      {/* <p className="mt-4 text-sm text-slate-600">{feature.description}</p> */}
+      <div
+        ref={progressTargetRef}
+        className="absolute bottom-0 left-0 h-[5px] w-full bg-slate-300"
+        hidden={!isActive || mobile}
+      ></div>
+      <div
+        ref={progressRef}
+        className={`bg-main-500 absolute bottom-0 left-0 h-[5px] transition-[width] duration-[4000ms] ease-linear ${
+          isActive && !mobile ? 'visible' : 'invisible'
+        }`}
+        style={{ width: isActive ? '100%' : '0%' }}
+      ></div>
+    </div>
+  )
+}
+
+function FeaturesMobile() {
+  return (
+    <div className="-mx-4 mt-16 flex flex-col gap-y-10 overflow-hidden px-4 sm:-mx-6 sm:px-6 lg:hidden">
+      {features.map((feature) => (
+        <div key={feature.summary}>
+          <Feature feature={feature} className="mx-auto max-w-2xl" isActive mobile/>
+          <div className="relative mt-10 pb-10">
+            <div className="absolute -inset-x-4 bottom-0 top-8 bg-slate-200 sm:-inset-x-6" />
+            <div className="relative mx-auto w-[52.75rem] overflow-hidden rounded-xl bg-white shadow-lg shadow-slate-900/5 ring-1 ring-slate-500/10">
+              <Image
+                className="w-full"
+                src={feature.image}
+                alt=""
+                sizes="52.75rem"
+              />
+            </div>
+          </div>
+        </div>
+      ))}
+    </div>
+  )
+}
+
+function FeaturesDesktop() {
+  return (
+    <Tab.Group as="div" className="hidden lg:mt-16 lg:block">
+      {({ selectedIndex }) => (
+        <>
+          <Tab.List className="mx-auto grid max-w-4xl grid-cols-3 gap-x-8">
+            {features.map((feature, featureIndex) => (
+              <Feature
+                key={feature.summary}
+                feature={{
+                  ...feature,
+                  name: (
+                    <Tab ref={feature.ref} className="outline-none">
+                      <span className="absolute inset-0" />
+                      {feature.name}
+                    </Tab>
+                  ),
+                }}
+                isActive={featureIndex === selectedIndex}
+                className="relative"
+                mobile={false}
+              />
+            ))}
+          </Tab.List>
+          <Tab.Panels className="relative mt-12 overflow-hidden rounded-4xl bg-slate-200 px-14 py-16 xl:px-16">
+            <div className="-mx-5 flex">
+              {features.map((feature, featureIndex) => (
+                <Tab.Panel
+                  static
+                  key={feature.summary}
+                  className={clsx(
+                    'px-5 transition duration-500 ease-in-out ui-not-focus-visible:outline-none',
+                    featureIndex !== selectedIndex && 'opacity-60',
+                  )}
+                  style={{ transform: `translateX(-${selectedIndex * 100}%)` }}
+                  aria-hidden={featureIndex !== selectedIndex}
+                >
+                  <div className="w-[52.75rem] overflow-hidden rounded-xl bg-white shadow-lg shadow-slate-900/5 ring-1 ring-slate-500/10">
+                    <Image
+                      className="w-full"
+                      src={feature.image}
+                      alt=""
+                      sizes="52.75rem"
+                    />
+                  </div>
+                </Tab.Panel>
+              ))}
+            </div>
+            <div className="pointer-events-none absolute inset-0 rounded-4xl ring-1 ring-inset ring-slate-900/10" />
+          </Tab.Panels>
+        </>
+      )}
+    </Tab.Group>
+  )
+}
+
+export function PrimaryFeatures() {
+  features.forEach((feature, index) => {
+    feature.ref = useRef<HTMLDivElement>(null)
+  })
+  return (
+    <section
+      id="primary-features"
+      aria-label="Features for simplifying everyday business tasks"
+      className="bg-slate-100/80 pb-14 pt-20 sm:pb-20 sm:pt-32 lg:pb-32"
+    >
+      <Container>
+        <div className="mx-auto max-w-2xl md:text-center">
+          <p className="text-main-500 font-display text-sm font-semibold sm:text-base">
+            HOW DOES IT WORK?
+          </p>
+          <h2 className="mt-2 max-w-2xl font-display text-3xl font-semibold text-slate-900 sm:text-4xl md:text-5xl">
+            Build voice agent with ease
           </h2>
-          <p className="mt-6 text-lg  text-blue-100">
-            Well everything you need if you aren’t that picky about minor
-            details like tax compliance.
+          <p className="mt-4 text-lg  text-slate-700">
+            Because you’d probably be a little confused if we suggested you
+            complicate your everyday business tasks instead.
           </p>
         </div>
-        <Tab.Group
-          as="div"
-          className="mt-16 grid grid-cols-1 items-center gap-y-2 pt-10 sm:gap-y-6 md:mt-20 lg:grid-cols-12 lg:pt-0"
-          vertical={tabOrientation === 'vertical'}
-        >
-          {({ selectedIndex }) => (
-            <>
-              <div className="-mx-4 flex overflow-x-auto pb-4 sm:mx-0 sm:overflow-visible sm:pb-0 lg:col-span-5">
-                <Tab.List className="relative z-10 flex gap-x-4 whitespace-nowrap px-4 sm:mx-auto sm:px-0 lg:mx-0 lg:block lg:gap-x-0 lg:gap-y-1 lg:whitespace-normal">
-                  {features.map((feature, featureIndex) => (
-                    <div
-                      key={feature.title}
-                      className={clsx(
-                        'group relative rounded-full px-4 py-1 lg:rounded-l-xl lg:rounded-r-none lg:p-6',
-                        selectedIndex === featureIndex
-                          ? 'bg-white lg:bg-white/10 lg:ring-1 lg:ring-inset lg:ring-white/10'
-                          : 'hover:bg-white/10 lg:hover:bg-white/5',
-                      )}
-                    >
-                      <h3>
-                        <Tab
-                          className={clsx(
-                            'font-display text-lg ui-not-focus-visible:outline-none',
-                            selectedIndex === featureIndex
-                              ? 'text-blue-600 lg:text-white'
-                              : 'text-blue-100 hover:text-white lg:text-white',
-                          )}
-                        >
-                          <span className="absolute inset-0 rounded-full lg:rounded-l-xl lg:rounded-r-none" />
-                          {feature.title}
-                        </Tab>
-                      </h3>
-                      <p
-                        className={clsx(
-                          'mt-2 hidden text-sm lg:block',
-                          selectedIndex === featureIndex
-                            ? 'text-white'
-                            : 'text-blue-100 group-hover:text-white',
-                        )}
-                      >
-                        {feature.description}
-                      </p>
-                    </div>
-                  ))}
-                </Tab.List>
-              </div>
-              <Tab.Panels className="lg:col-span-7">
-                {features.map((feature) => (
-                  <Tab.Panel key={feature.title} unmount={false}>
-                    <div className="relative sm:px-6 lg:hidden">
-                      <div className="absolute -inset-x-4 bottom-[-4.25rem] top-[-6.5rem] bg-white/10 ring-1 ring-inset ring-white/10 sm:inset-x-0 sm:rounded-t-xl" />
-                      <p className="relative mx-auto max-w-2xl text-base text-white sm:text-center">
-                        {feature.description}
-                      </p>
-                    </div>
-                    <div className="mt-10 w-[45rem] overflow-hidden rounded-xl bg-slate-50 shadow-xl shadow-blue-900/20 sm:w-auto lg:mt-0 lg:w-[67.8125rem]">
-                      <Image
-                        className="w-full"
-                        src={feature.image}
-                        alt=""
-                        priority
-                        sizes="(min-width: 1024px) 67.8125rem, (min-width: 640px) 100vw, 45rem"
-                      />
-                    </div>
-                  </Tab.Panel>
-                ))}
-              </Tab.Panels>
-            </>
-          )}
-        </Tab.Group>
+        <FeaturesMobile />
+        <FeaturesDesktop />
       </Container>
     </section>
   )
