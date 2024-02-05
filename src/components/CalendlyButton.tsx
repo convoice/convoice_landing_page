943b2f8c5745e7deb64dc7b05d1223a1c8df6f61
Yuxing <tomzhouyuxing@outlook.com>
2024-02-04 22:55:23 -0800

    changed demo calendly link to tom's

--- src/components/CalendlyButton.tsx
+++ src/components/CalendlyButton.tsx
@@ -23,8 +23,12 @@ export default function Calendly({
         primaryColor: "00a2ff",
         textColor: "4d5055",
       }}
-      className={`transition duration-200 active:scale-[97%] ${variant === "main" ? 'rounded-lg border-[2px] border-main-500 px-6 py-2.5 text-xl font-semibold text-main-500 text-main-500 hover:text-main-600 hover:border-main-600 focus-visible:outline-main-600' : 'rounded-lg border-[2px] border-white text-white hover:text-white/80 hover:border-white/80 focus-visible:outline-white text-main-500 sm:text-lg items-center justify-center rounded-lg py-1.5 px-4 font-medium focus:outline-none focus-visible:outline-2 focus-visible:outline-offset-2'}`}
-      url="https://calendly.com/convoice"
+      className={`transition duration-200 active:scale-[97%] ${
+        variant === "main"
+          ? "rounded-lg border-[2px] border-main-500 px-6 py-2.5 text-xl font-semibold text-main-500 text-main-500 hover:border-main-600 hover:text-main-600 focus-visible:outline-main-600"
+          : "items-center justify-center rounded-lg rounded-lg border-[2px] border-white px-4 py-1.5 font-medium text-main-500 text-white hover:border-white/80 hover:text-white/80 focus:outline-none focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-white sm:text-lg"
+      }`}
+      url="https://calendly.com/yuxing-convoice/chat-w-convoice-team"
       rootElement={rootElement as HTMLElement} // Update the type of rootElement prop
       text="Book a Demo"
     />
